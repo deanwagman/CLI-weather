cb4ad6c2517b7bf37cbeed6a76e06566cd56cf0d
Dean Wagman <dean@purplerockscissors.com>
2015-11-25 10:31:23 -0500

    Remove API key from repo, put in config file

--- weather.js
+++ weather.js
@@ -1,12 +1,10 @@
 
 // Modules
+var CONFIG = require('./config');
 var request = require('request');
 
-// Variables
-var API_KEY = '50a35d629a1860b4f62e371bcd566941';
-
 module.exports = function(city, geolocation) {
-	var url = `https://api.forecast.io/forecast/${API_KEY}/${geolocation}`;
+	var url = `https://api.forecast.io/forecast/${CONFIG.WEATHER_API_KEY}/${geolocation}`;
 
 	return new Promise(function (resolve, reject) {
 		request({url: url, json: true}, function(error, response, body) {
